ef816111e2f1d4484a749889bb6a8b5dd65baab0
Masahiko Isshiki <masahiko_isshiki@yahoo.co.jp>
2024-07-21 11:07:33 +0900

    changed to Taskbar app

--- WinSleepWell/App.xaml.cs
+++ WinSleepWell/App.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Security.Principal;
 using System.Windows;
+using Application = System.Windows.Application;
+using MessageBox = System.Windows.MessageBox;
 
 namespace WinSleepWell
 {
     public partial class App : Application
     {
+        public MainWindow mainWindow;
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
@@ -15,6 +19,8 @@ namespace WinSleepWell
                 MessageBox.Show("This application must be run as an administrator.", "Insufficient Privileges", MessageBoxButton.OK, MessageBoxImage.Error);
                 Application.Current.Shutdown();
             }
+
+            mainWindow = new MainWindow();
         }
 
         private bool IsAdministrator()
@@ -22,6 +28,11 @@ namespace WinSleepWell
             var identity = WindowsIdentity.GetCurrent();
             var principal = new WindowsPrincipal(identity);
             return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
         }
     }
 }
